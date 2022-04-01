6213fc7a09b74edcc6f40fbc92adbc7ee9126c4b
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-04-01 22:11:34 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (81.55%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -3,15 +3,13 @@ public:
     int minStartValue(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        int mini=INT_MAX;
+        int mini=0;
         for(int &x:nums){
             sum+=x;
             mini=min(mini,sum);
         }
-        if(mini<0){
-            return (abs(mini)+1);
-        }
-        return 1;
+
+        return 1-mini;
         
     }
 };
