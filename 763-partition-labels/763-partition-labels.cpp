3ccda4908a3aa47cb8de98a055eab7b03e4a0292
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-04-05 09:41:24 +0530

    Time: 4 ms (69.25%), Space: 6.6 MB (59.83%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,23 +1,61 @@
 class Solution {
 public:
-    vector<int> partitionLabels(string s) {
-        int n=s.size();
-        int cnt=0;
-        int lli=0;
-        vector<int>res;
-        for(int i=0;i<n;i++){
-            cnt++;
-            for(int j=n-1;j>=0;j--){
-                if(s[i]==s[j]){
-                    lli=max(lli,j);
-                    break;
-                }
-            }
-            if(i==lli){
-                res.push_back(cnt);
-                cnt=0;
+    vector<int> partitionLabels(string S) {
+        //O(n^2)
+        //int n=s.size();
+        // int cnt=0;
+        // int lli=0;
+        // vector<int>res;
+        // for(int i=0;i<n;i++){
+        //     cnt++;
+        //     for(int j=n-1;j>=0;j--){
+        //         if(s[i]==s[j]){
+        //             lli=max(lli,j);
+        //             break;
+        //         }
+        //     }
+        //     if(i==lli){
+        //         res.push_back(cnt);
+        //         cnt=0;
+        //     }
+        // }
+        // return res;
+        
+        //O(n)
+    //     int n=s.size();
+    //     vector<int>pos(26,0);
+    //     for(int i=0;i<n;i++){
+    //         pos[s[i]-'a']=i;
+    //     }
+    //     vector<int>res;
+    //     int cnt=0 ,lli=0,clli=0;
+    //     for(int i=0;i<n;i++){
+    //         clli=pos[s[i]-'a'];
+    //         lli=max(lli,clli);
+    //         cnt++;
+    //         if(lli==i){
+    //             res.push_back(cnt);
+    //             cnt=0;
+    //         }
+    //     }
+    //     return res;
+    // }
+        //O(N) neat code 
+        vector<int> charIdx(26, 0);
+        for(int i = 0; i < S.size(); i++){
+            charIdx[S[i]-'a'] = i;
+        }
+        
+        vector<int> results;
+        
+        int maxIdx = -1, lastIdx = 0;
+        for(int i = 0; i < S.size(); i++){
+            maxIdx = max(maxIdx, charIdx[S[i]-'a']);
+            if(i == maxIdx) {
+                results.push_back(maxIdx - lastIdx + 1);
+                lastIdx = i+1;
             }
         }
-        return res;
+        return results;
     }
 };
