8316fe3616f4ee5f7971a7cbdb6d0a38de290eb0
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-04-09 14:57:31 +0530

    Time: 220 ms (17.05%), Space: 52.3 MB (14.25%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.cpp
+++ 888-fair-candy-swap/888-fair-candy-swap.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
-    vector<int> fairCandySwap(vector<int>& a, vector<int>& b) {
+    vector<int> fairCandySwap(vector<int>& A, vector<int>& B) {
         // int n1=aliceSizes.size();
         // int n2=bobSizes.size();
         
+//         int asum=0,bsum=0;
+//         for(auto &x:a){
+//             asum+=x;
+//         }
+//         for(auto &x:b){
+//             bsum+=x;
+//         }
+//       for(auto &x:a){
+//           int sum1=asum;
+//           int sum2=bsum;
+//           sum1-=x;
+//           sum2+=x;
+//           for(auto &y:b){
+//               sum2-=y;
+//               sum1+=y;
+//               if(sum1==sum2){
+//                   return {x,y};
+//               }
+//             sum1=asum;
+//             sum2=bsum;
+//             sum1-=x;
+//             sum2+=x;
+              
+//           }  
+//       }
+//      return {0,0};   
         int asum=0,bsum=0;
-        for(auto &x:a){
+        for(int &x:A){
             asum+=x;
         }
-        for(auto &x:b){
-            bsum+=x;
+         for(int &y:B){
+            bsum+=y;
         }
-      for(auto &x:a){
-          int sum1=asum;
-          int sum2=bsum;
-          sum1-=x;
-          sum2+=x;
-          for(auto &y:b){
-              sum2-=y;
-              sum1+=y;
-              if(sum1==sum2){
-                  return {x,y};
-              }
-            sum1=asum;
-            sum2=bsum;
-            sum1-=x;
-            sum2+=x;
-              
-          }  
-      }
-     return {0,0};   
+        int delta=(asum-bsum)/2;
+        
+        set<int>searchA;
+        for(auto &x:A){
+            searchA.insert(x);
+        }
+        for(auto &y:B){
+            if(searchA.count(delta+y)>0){
+                return {delta+y,y};
+            }
+        }
+        return {};
+        
     }
 };
