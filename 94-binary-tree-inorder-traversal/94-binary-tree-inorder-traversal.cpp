395591a951cbdc054f4afa92551c6f08c782aac1
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-04-23 08:31:13 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (17.71%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -25,7 +25,23 @@ public:
         if(root==NULL){
             return ans;
         }
-        inorder(ans,root);
+        // inorder(ans,root);
+        stack<TreeNode*>st;
+        while(true){
+            if(root!=NULL){
+                st.push(root);
+                root=root->left;
+            }
+            else{
+                if(st.empty()==true){
+                    break;
+                }
+                root=st.top();
+                st.pop();
+                ans.push_back(root->val);
+                root=root->right;
+            }
+        }
         return ans;
     }
 };
