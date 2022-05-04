dbf1f9350a8d4855ea77f2ccc4f87d0a0091014b
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-05-04 21:59:30 +0530

    Time: 30 ms (5.95%), Space: 13.7 MB (19.38%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,95 @@
 class Solution {
+private:
+    int leftIndex(vector<int>& nums, int target){
+        int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        int ans=-1;
+        while(s<=e){
+            if(nums[mid]==target){
+                ans=mid;
+                e=mid-1;
+            }
+            else if(nums[mid]>target) e=mid-1;
+            
+            else s=mid+1;  
+            
+             mid=s+(e-s)/2;
+        }
+        return ans;
+    }
+    int rightIndex(vector<int>& nums, int target){
+         int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        int ans=-1;
+        while(s<=e){
+            if(nums[mid]==target){
+                ans=mid;
+                s=mid+1;
+            }
+            else if(nums[mid]>target){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return ans;
+    }
 public:
-    int leftElement(vector<int>& nums, int target){
-       
-        int n=nums.size(), l=0,r=n-1,ans=-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                r=mid-1;
-            }
-            else if(nums[mid]>target){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return ans;
-        
-    }
-    int rightElement(vector<int>& nums, int target){
-        int n=nums.size(), l=0,r=n-1,ans=-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                l=mid+1;
-            }
-            else if(nums[mid]>target){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return ans;
-    }
-    vector<int> searchRange(vector<int>& nums, int target) {
-         if(nums.size()==0){
+    vector<int> searchRange(vector<int>& nums, int target) {        
+        if(nums.size()==0){
             return {-1,-1};
         }
-        vector<int>res={leftElement(nums,target),rightElement(nums,target)};
-        return res;
-        
+        vector<int>res;
+        res.push_back(leftIndex(nums,target));
+        res.push_back(rightIndex(nums,target));
+        return res;     
     }
 };
+    //     int leftElement(vector<int>& nums, int target){
+       
+//         int n=nums.size(), l=0,r=n-1,ans=-1;
+//         while(l<=r){
+//             int mid=l+(r-l)/2;
+//             if(nums[mid]==target){
+//                 ans=mid;
+//                 r=mid-1;
+//             }
+//             else if(nums[mid]>target){
+//                 r=mid-1;
+//             }
+//             else{
+//                 l=mid+1;
+//             }
+//         }
+//         return ans;
+        
+//     }
+//     int rightElement(vector<int>& nums, int target){
+//         int n=nums.size(), l=0,r=n-1,ans=-1;
+//         while(l<=r){
+//             int mid=l+(r-l)/2;
+//             if(nums[mid]==target){
+//                 ans=mid;
+//                 l=mid+1;
+//             }
+//             else if(nums[mid]>target){
+//                 r=mid-1;
+//             }
+//             else{
+//                 l=mid+1;
+//             }
+//         }
+//         return ans;
+//     }
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//          if(nums.size()==0){
+//             return {-1,-1};
+//         }
+//         vector<int>res={leftElement(nums,target),rightElement(nums,target)};
+//         return res;
+        
+//     }
