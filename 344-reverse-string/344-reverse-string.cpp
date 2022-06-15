a19c61915b08e290d79bc1179a2e269778f20369
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-06-15 17:18:03 +0530

    Time: 23 ms (84.65%), Space: 23.3 MB (5.01%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,15 +1,26 @@
 class Solution {
+private:
+    void solve(vector<char>& s,int i,int j){
+        if(i>j) return;
+        swap(s[i],s[j]);
+        solve(s,i+1,j-1);
+    }
 public:
     void reverseString(vector<char>& s) {
         int i=0;
         int j=s.size()-1;
-        while(i<j){
-            s[i]^=s[j];
-            s[j]^=s[i];
-            s[i]^=s[j];
-            i++;j--;
-        }
-        return ;
+        solve(s,i,j);
+        
+        //using xor operator 
+        // int i=0;
+        // int j=s.size()-1;
+        // while(i<j){
+        //     s[i]^=s[j];
+        //     s[j]^=s[i];
+        //     s[i]^=s[j];
+        //     i++;j--;
+        // }
+        // return ;
             
     }
 };
