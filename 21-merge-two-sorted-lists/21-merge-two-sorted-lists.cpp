ae5268a80bd5d4a1efa303421985b9159a57c268
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-06-22 18:07:42 +0530

    Time: 16 ms (26.45%), Space: 14.8 MB (42.74%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,46 +11,79 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* pseudo_head=new ListNode(-101);
+        pseudo_head->next=l1;
         ListNode* h1=l1;
         ListNode* h2=l2;
-        ListNode* h3;
-        ListNode* t;
-        if(!h1){
-            return h2;
-        }
-        if(!h2){
-            return h1;
-        }
-        if(h1->val< h2->val){
-            h3=h1;
-            t=h1;
-            h1=h1->next;
-        }
-        else{
-            h3=h2;
-            t=h2;
-            h2=h2->next;
-        }
-        while(h1!=NULL and h2!=NULL){
+        ListNode* h3=pseudo_head;
+        
+        while(h1 && h2){
             if(h1->val<h2->val){
-                t->next=h1;
-                t=h1;
+                h3->next=h1;
+                h3=h1;
                 h1=h1->next;
             }
             else{
-                t->next=h2;
-                t=h2;
+                h3->next=h2;
+                h3=h2;
                 h2=h2->next;
             }
-        }
-        if(h1){
-            t->next=h1;
             
         }
-        if(h2){
-            t->next=h2;
+        if(h1){
+            h3->next=h1;
+        }else{
+            h3->next=h2;
         }
-        return h3;
+        ListNode* head=pseudo_head->next;
+        pseudo_head->next=NULL;
+        delete pseudo_head;
+        return head;
         
     }
+    
+    
+    
+//         ListNode* h1=l1;
+//         ListNode* h2=l2;
+//         ListNode* h3;
+//         ListNode* t;
+//         if(!h1){
+//             return h2;
+//         }
+//         if(!h2){
+//             return h1;
+//         }
+//         if(h1->val< h2->val){
+//             h3=h1;
+//             t=h1;
+//             h1=h1->next;
+//         }
+//         else{
+//             h3=h2;
+//             t=h2;
+//             h2=h2->next;
+//         }
+//         while(h1!=NULL and h2!=NULL){
+//             if(h1->val<h2->val){
+//                 t->next=h1;
+//                 t=h1;
+//                 h1=h1->next;
+//             }
+//             else{
+//                 t->next=h2;
+//                 t=h2;
+//                 h2=h2->next;
+//             }
+//         }
+//         if(h1){
+//             t->next=h1;
+            
+//         }
+//         if(h2){
+//             t->next=h2;
+//         }
+//         return h3;
+        
+//     }
 };
