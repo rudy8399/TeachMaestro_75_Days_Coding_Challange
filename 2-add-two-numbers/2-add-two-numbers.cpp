33962b2002d8c9c07f6d0a9384a0272ce3f0d115
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-06-27 16:49:43 +0530

    Time: 50 ms (59.40%), Space: 71.3 MB (80.95%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,60 +11,84 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode* h1=l1,*h2=l2,*h3=NULL,* ptr=NULL;
-        int rem=0,sum=0,carry=0;
-        
-        while(h1 && h2){
-            sum=h1->val+h2->val+carry;
-            rem=sum%10;
+        ListNode *dummy=new ListNode();
+        ListNode* temp=dummy;
+        int carry=0;
+        while(l1 || l2 || carry){
+            int sum=0;
+            if(l1){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum+=carry;
             carry=sum/10;
-            ListNode* n=new ListNode(rem);
-            if(h3==NULL){
-                h3=n;
-                ptr=n;
-            }
-            else{
-                ptr->next=n;
-                ptr=n;
-            }
-            h1=h1->next;
-            h2=h2->next;
+            
+            ListNode* node=new ListNode(sum%10);
+            temp->next=node;
+            temp=temp->next;
         }
-        while(h1){
-            sum=h1->val+carry;
-            rem=sum%10;
-            carry=sum/10;
-            ListNode* n=new ListNode(rem);
-            if(h3==NULL){
-                h3=n;
-                ptr=n;
-            }
-            else{
-                ptr->next=n;
-                ptr=n;
-            }
-            h1=h1->next;
-        }
-         while(h2){
-            sum=h2->val+carry;
-            rem=sum%10;
-            carry=sum/10;
-            ListNode* n=new ListNode(rem);
-            if(h3==NULL){
-                h3=n;
-                ptr=n;
-            }
-            else{
-                ptr->next=n;
-                ptr=n;
-            }
-            h2=h2->next;
-        }
-        if(carry>0){
-         ListNode* n=new ListNode(carry);
-        ptr->next=n;
-        }
-        return h3;
+        return dummy->next;
     
     }
+        //     ListNode* h1=l1,*h2=l2,*h3=NULL,* ptr=NULL;
+//         int rem=0,sum=0,carry=0;
+        
+//         while(h1 && h2){
+//             sum=h1->val+h2->val+carry;
+//             rem=sum%10;
+//             carry=sum/10;
+//             ListNode* n=new ListNode(rem);
+//             if(h3==NULL){
+//                 h3=n;
+//                 ptr=n;
+//             }
+//             else{
+//                 ptr->next=n;
+//                 ptr=n;
+//             }
+//             h1=h1->next;
+//             h2=h2->next;
+//         }
+//         while(h1){
+//             sum=h1->val+carry;
+//             rem=sum%10;
+//             carry=sum/10;
+//             ListNode* n=new ListNode(rem);
+//             if(h3==NULL){
+//                 h3=n;
+//                 ptr=n;
+//             }
+//             else{
+//                 ptr->next=n;
+//                 ptr=n;
+//             }
+//             h1=h1->next;
+//         }
+//          while(h2){
+//             sum=h2->val+carry;
+//             rem=sum%10;
+//             carry=sum/10;
+//             ListNode* n=new ListNode(rem);
+//             if(h3==NULL){
+//                 h3=n;
+//                 ptr=n;
+//             }
+//             else{
+//                 ptr->next=n;
+//                 ptr=n;
+//             }
+//             h2=h2->next;
+//         }
+//         if(carry>0){
+//          ListNode* n=new ListNode(carry);
+//         ptr->next=n;
+//         }
+//         return h3;
+    
+//     }
+        
 };
