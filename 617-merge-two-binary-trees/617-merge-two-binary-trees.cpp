e96268a010e8a0951190cadd163b1cc9be0c63eb
rudy8399 <65102463+rudy8399@users.noreply.github.com>
2022-07-27 15:37:46 +0530

    Time: 61 ms (50.06%), Space: 32.4 MB (62.07%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -12,15 +12,47 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* r1, TreeNode* r2) {
-        if(r1==NULL){
-            return r2;
+        if(r1==NULL) return r2;
+        if(r2==NULL) return r1;
+        
+        int sum=0;
+        if(r1){
+            sum+=r1->val;
         }
-        if(r2==NULL){
-            return r1;
+        if(r2){
+            sum+=r2->val;
         }
-        r1->val+=r2->val;
+        r1->val=sum;
         r1->left=mergeTrees(r1->left,r2->left);
         r1->right=mergeTrees(r1->right,r2->right);
         return r1;
+        
     }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //     if(r1==NULL){
+    //         return r2;
+    //     }
+    //     if(r2==NULL){
+    //         return r1;
+    //     }
+    //     r1->val+=r2->val;
+    //     r1->left=mergeTrees(r1->left,r2->left);
+    //     r1->right=mergeTrees(r1->right,r2->right);
+    //     return r1;
+    // }
 };
